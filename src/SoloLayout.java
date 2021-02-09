d81500c4139d5f4194ada9c90caaded5b21789d1
filip.norberg <filipnorberg02@outlook.com>
2021-02-09 13:18:57 +0100

    created viewform

--- src/SoloLayout.java
+++ src/SoloLayout.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class SoloLayout {
+    private JLabel Soloäventyr;
+    private JTextPane Storytext;
+    private JButton Button1;
+    private JButton Button2;
+    private JTextPane Option1;
+    private JTextPane Option2;
+    private JButton Reset;
 }
